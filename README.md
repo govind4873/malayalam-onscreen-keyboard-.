2794921567e42b9605ede0e54699771f280e0bd1
govind <govind4873@gmail.com>
2018-07-03 17:35:27 +0530

    the final commit after the keyboard layout and functions

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # malayalam-virtual-keyboard-.
 This project aims to develop an onscreen keyboard for disabled in Malayalam language for Linux platforms  using GTK libraries .
 I am learning the basics of Gnome application development from https://developer.gnome.org/gtk3/3.22/ 
-step by step development of an onscreen keyboard . by Goind GS 
+step by step development of an onscreen keyboard . 
+ 
+the present status of the project , the keyboard can input UNICODE characters of malayalam in the terminal window .
+gcc `pkg-config --cflags gtk+-3.0` -o example-3 example-3.c `pkg-config --libs gtk+-3.0`
+ 
+Scope of improvement 
+1. finding a method to input the malayalam text to the input stream and not from the terminal 
+2. code optimization of the keyboard, instead of individual intialisation of objects 
+3. function to return the unicode from the button label instead of triggering individual functions to return the unicode.
+
+by Goind GS, Btech student,Sree Chitra Thirunal College of Engineering 
+
--- example-3.c
+++ example-3.c
@@ -1,17 +1,499 @@
+// compile using the below command 
+// gcc `pkg-config --cflags gtk+-3.0` -o example-3 example-3.c `pkg-config --libs gtk+-3.0`
+
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <wchar.h>
 #include <locale.h>
 
 static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
+// functions that return the unicode characters with the button click event of each button 
+print_1()
 {
     setlocale(LC_CTYPE, "");
-    wchar_t star = 0x0D05;
-    wprintf(L"%lc\n", star);
+    wchar_t word =0x0D05; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D06; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D07; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D08; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D09; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_6()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D0A; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_7()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D0B; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_8()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D0E; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_9()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D0F; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_10()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D10; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_11()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D12; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_14()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D3E; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_15()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D3F; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_16()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D40; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_1()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D15; ;
+    wprintf(L"%lc", word);
+}
+/////////////////////
+static void
+print_2_2()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D16; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_3()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D17; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_4()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D18; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_5()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D19; ;
+    wprintf(L"%lc", word);
 }
 
+static void
+print_2_7()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D1A; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_8()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D1B; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_9()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D1C; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_10()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D1D; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_11()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D1E; ;
+    wprintf(L"%lc", word);
+}
+
+static void
+print_2_14()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D41; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_15()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D42; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_2_16()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D43; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_1()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D1F; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_2()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D20; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_3()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D21; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_4()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D22; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_5()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D23; ;
+    wprintf(L"%lc", word);
+}
+
+static void
+print_3_7()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D24; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_8()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D25; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_9()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D26; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_10()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D27; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_11()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D28; ;
+    wprintf(L"%lc", word);
+}
+
+static void
+print_3_14()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D46; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_15()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D47; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_3_16()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D48; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_1()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D2A; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_2()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D2B; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_3()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D2C; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_4()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D2D; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_5()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D2E; ;
+    wprintf(L"%lc", word);
+}
+
+static void
+print_4_7()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D7A; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_8()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D7B; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_9()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D7C; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_10()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D7D; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_11()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D7E; ;
+    wprintf(L"%lc", word);
+}
+
+static void
+print_4_14()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D4D; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_15()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D4B; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_4_16()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D4C; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_1()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D2F; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_2()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D30; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_3()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D32; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_4()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D35; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_5()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D36; ;
+    wprintf(L"%lc", word);
+}
+
+static void
+print_5_6()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D37; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_7()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D38; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_8()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D39; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_9()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D33; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_10()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D34; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_11()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D31; ;
+    wprintf(L"%lc", word);
+}
+
+static void
+print_5_14()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x002C; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_15()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D57; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_5_16()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0D02; ;
+    wprintf(L"%lc", word);
+}
+static void
+print_6_1()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t word =0x0020; ;
+    wprintf(L"%lc", word);
+}
+
+// main fuunction 
 int
 main (int   argc,
       char *argv[])
@@ -25,44 +507,248 @@ main (int   argc,
 
   /* Construct a GtkBuilder instance and load our UI description */
   builder = gtk_builder_new ();
-  if (gtk_builder_add_from_file (builder, "builder.ui", &error) == 0)
+  if (gtk_builder_add_from_file (builder, "builder.ui", &error) == 0) // the design layout is in the file builder.ui 
     {
       g_printerr ("Error loading file: %s\n", error->message);
       g_clear_error (&error);
       return 1;
     }
 
-  /* Connect signal handlers to the constructed widgets. */
+  /* Connect signal handlers to the constructed widgets. 
+     */
   window = gtk_builder_get_object (builder, "window");
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-  gtk_window_set_default_size (GTK_WINDOW (window), 700, 200);
 
   button = gtk_builder_get_object (builder, "button1");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_1), NULL);
 
   button = gtk_builder_get_object (builder, "button2");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2), NULL);
   
   button = gtk_builder_get_object (builder, "button3");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3), NULL);
 
   button = gtk_builder_get_object (builder, "button4");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4), NULL);
  
   button = gtk_builder_get_object (builder, "button5");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5), NULL);
  
  button = gtk_builder_get_object (builder, "button6");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_6), NULL);
  
  button = gtk_builder_get_object (builder, "button7");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_7), NULL);
 
  button = gtk_builder_get_object (builder, "button8");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (print_8), NULL);
 
-  button = gtk_builder_get_object (builder, "quit");
-  g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+  button = gtk_builder_get_object (builder, "button9");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_9), NULL);
+
+  button = gtk_builder_get_object (builder, "button10");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_10), NULL);
+
+  button = gtk_builder_get_object (builder, "button11");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_11), NULL);
+
+  button = gtk_builder_get_object (builder, "button12");
+ 
+  button = gtk_builder_get_object (builder, "button9");
+  
+
+  button = gtk_builder_get_object (builder, "button14");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_14), NULL);
+
+  button = gtk_builder_get_object (builder, "button15");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_15), NULL);
+
+  button = gtk_builder_get_object (builder, "button16");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_16), NULL);
+  
+  button = gtk_builder_get_object (builder, "button2_1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_1), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_2), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_3");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_3), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_4");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_4), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_5");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_5), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_6");
+
+  button = gtk_builder_get_object (builder, "button2_7");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_7), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_8");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_8), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_9");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_9), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_10");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_10), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_11");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_11), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_12");
+
+  button = gtk_builder_get_object (builder, "button2_13");
+
+  button = gtk_builder_get_object (builder, "button2_14");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_14), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_15");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_15), NULL);
+
+  button = gtk_builder_get_object (builder, "button2_16");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_2_16), NULL);
+
+   button = gtk_builder_get_object (builder, "button3_1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_1), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_2), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_3");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_3), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_4");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_4), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_5");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_5), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_6");
+
+  button = gtk_builder_get_object (builder, "button3_7");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_7), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_8");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_8), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_9");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_9), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_10");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_10), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_11");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_11), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_12");
+
+  button = gtk_builder_get_object (builder, "button3_13");
+
+  button = gtk_builder_get_object (builder, "button3_14");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_14), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_15");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_15), NULL);
+
+  button = gtk_builder_get_object (builder, "button3_16");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_3_16), NULL);
+  
+ button = gtk_builder_get_object (builder, "button4_1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_1), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_2), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_3");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_3), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_4");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_4), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_5");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_5), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_6");
+
+  button = gtk_builder_get_object (builder, "button4_7");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_7), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_8");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_8), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_9");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_9), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_10");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_10), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_11");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_1), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_12");
+
+  button = gtk_builder_get_object (builder, "button4_13");
+
+  button = gtk_builder_get_object (builder, "button4_14");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_14), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_15");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_15), NULL);
+
+  button = gtk_builder_get_object (builder, "button4_16");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_4_16), NULL);
+  
+  button = gtk_builder_get_object (builder, "button5_1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_1), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_2), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_3");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_3), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_4");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_4), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_5");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_5), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_6");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_6), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_7");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_7), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_8");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_8), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_9");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_9), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_10");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_10), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_11");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_11), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_12");
+
+  button = gtk_builder_get_object (builder, "button5_13");
+
+  button = gtk_builder_get_object (builder, "button5_14");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_14), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_15");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_15), NULL);
+
+  button = gtk_builder_get_object (builder, "button5_16");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_5_16), NULL);
+
+  button = gtk_builder_get_object (builder, "button6_1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_6_1), NULL);
 
   gtk_main ();
 
